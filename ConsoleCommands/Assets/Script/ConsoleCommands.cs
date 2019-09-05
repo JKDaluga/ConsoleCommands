858d9d7a4d353cbcfd77586030549f11a9c90cb7
Chase Kurkowski <chk@fastmail.com>
2019-09-05 17:02:10 -0500

    Console Commands script and Scene added

---
+++ ConsoleCommands/Assets/Script/ConsoleCommands.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Console
+{
+    public abstract class ConsoleCommand
+    {
+        public abstract string Name { get; protected set; }
+        public abstract string Command { get; protected set; }
+        public abstract string Description { get; protected set; }
+        public abstract string Help { get; protected set; }
+
+        public void AddCommandToConsole()
+        {
+            string addMessage = " command has been added to the console.";
+
+            DevConsole.AddCommandsToConsole(Command, this);
+            Debug.Log(Name + addMessage);
+        }
+
+        public abstract void RunCommand();
+    }
+
+    public class CommandQuit : ConsoleCommand
+    {
+        public override string Name { get; protected set; }
+        public override string Command { get; protected set; }
+        public override string Description { get; protected set; }
+        public override string Help { get; protected set; }
+
+        public CommandQuit()
+        {
+            Name = "Quit";
+            Command = "quit";
+            Description = "Quits the application";
+            Help = "Use this command with no arguments to force Unity to quit!";
+
+            AddCommandToConsole();
+        }
+
+        public override void RunCommand()
+        {
+            if (Application.isEditor)
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+
+        public static CommandQuit CreateCommand()
+        {
+            return new CommandQuit();
+        }
+    }
+}
--- ConsoleCommands/Assets/Script/DevConsole.cs
+++ ConsoleCommands/Assets/Script/DevConsole.cs
@@ -1,18 +1,110 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class DevConsole : MonoBehaviour
+namespace Console
 {
-    // Start is called before the first frame update
-    void Start()
+    public class DevConsole : MonoBehaviour
     {
-        
-    }
+        public static DevConsole Instance { get; private set; }
+        public static Dictionary<string, ConsoleCommand> Commands { get; private set; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("UI Components")]
+        public Canvas consoleCanvas;
+        public Text consoleText;
+        public Text inputText;
+        public InputField consoleInput;
+
+        private void Awake()
+        {
+            if(Instance != null)
+            {
+                return;
+            }
+
+            Instance = this;
+            Commands = new Dictionary<string, ConsoleCommand>();
+        }
+
+        private void Start()
+        {
+            consoleCanvas.gameObject.SetActive(false);
+            CreateCommands();
+        }
+
+        private void OnEnable()
+        {
+            Application.logMessageReceived += HandleLog;
+        }
+
+        private void OnDisable()
+        {
+            Application.logMessageReceived -= HandleLog;
+        }
+
+        private void HandleLog(string logMessage, string stackTrace, LogType type)
+        {
+            string _message = "[" + type.ToString() + "] " + logMessage;
+            AddMessageToConsole(_message);
+        }
+
+        private void CreateCommands()
+        {
+            CommandQuit.CreateCommand();
+        }
+
+        public static void AddCommandsToConsole(string _name, ConsoleCommand _command)
+        {
+            if(!Commands.ContainsKey(_name))
+            {
+                Commands.Add(_name, _command);
+            }
+        }
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.BackQuote))
+            {
+                consoleCanvas.gameObject.SetActive(!consoleCanvas.gameObject.activeInHierarchy);
+            }
+
+            if(consoleCanvas.gameObject.activeInHierarchy)
+            {
+                if(Input.GetKeyDown(KeyCode.Return))
+                {
+                    if(inputText.text != "")
+                    {
+                        AddMessageToConsole(inputText.text);
+                        ParseInput(inputText.text);
+                    }
+                }
+            }
+        }
+
+        private void AddMessageToConsole(string msg)
+        {
+            consoleText.text += msg + "\n";
+        }
+
+        private void ParseInput(string input)
+        {
+            string[] _input = input.Split(null);
+
+            if (_input.Length == 0 || _input == null)
+            {
+                Debug.LogWarning("Command not recognized.");
+                return;
+            }
+
+            if (!Commands.ContainsKey(_input[0]))
+            {
+                Debug.LogWarning("Command not recognized.");
+            }
+            else
+            {
+                Commands[_input[0]].RunCommand();
+            }
+        }
     }
 }
